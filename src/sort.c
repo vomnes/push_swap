cba6ee2be9888d457323f9f7b4d018381b98d8e9
Valentin OMNES <vomnes@student.42.fr>
2017-02-20 14:43:03 +0100

    Sort works

--- src/sort.c
+++ src/sort.c
@@ -7,6 +7,8 @@ int ft_lst_min(t_node *lst, t_env *env)
 
   temp = lst;
   index = 0;
+  env->data_a.min = 2147483647;
+  env->data_a.index_min = 1;
   while (temp != NULL)
   {
     env->data_a.index_min = (int)temp->data < env->data_a.min ?
@@ -22,24 +24,50 @@ int ft_lst_min(t_node *lst, t_env *env)
 int ft_sort(t_env *env)
 {
   int middle;
+  int complexity;
 
-  env->data_a.min = 2147483647;
-  env->data_a.index_min = 1;
   middle = 0;
+  complexity = 0;
   while (env->stack_a != NULL)
   {
       env->data_a.len = ft_lst_min(env->stack_a, env);
-      middle = env->data_a.len / env->data_a.index_min;
+      middle = (int)(env->data_a.len / 2);
+      ft_printf("middle = %d | len = %d | MIN = %d | index = %d\n",
+      middle, env->data_a.len, env->data_a.min, env->data_a.index_min);
       if (env->data_a.index_min == 0)
-          ft_push_ab(&env->stack_b, &env->stack_a);
+      {
+        ft_push_ab(&env->stack_a, &env->stack_b);
+        complexity++;
+        ft_putendl("pb");
+      }
+      else if (env->data_a.index_min == 1)
+      {
+        ft_swap_one_two(&env->stack_a);
+        complexity++;
+        ft_putendl("sa");
+      }
       else if (env->data_a.index_min < middle)
-          ft_rotate(&env->stack_a);
+      {
+        ft_rotate(&env->stack_a);
+        complexity++;
+        ft_putendl("ra");
+      }
       else
-          ft_reverse_rotate(&env->stack_a);
-      ft_lst_display(env->stack_a);
-      ft_putendl("========[OK]========");
+      {
+        ft_reverse_rotate(&env->stack_a);
+        complexity++;
+        ft_putendl("rra");
+      }
   }
-  ft_printf("len = %d | MIN = %d | index = %d\n",
-  env->data_a.len, env->data_a.min, env->data_a.index_min);
+  while (env->stack_b != NULL)
+  {
+      ft_push_ab(&env->stack_b, &env->stack_a);
+      complexity++;
+      ft_putendl("pa");
+  }
+//  ft_printf("len = %d | MIN = %d | index = %d\n",
+//  env->data_a.len, env->data_a.min, env->data_a.index_min);
+  ft_printf("complexity >> %d\n", complexity);
+//  ft_lst_display(env->stack_a);
   return (0);
 }
