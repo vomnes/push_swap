29bec2d4226d0c6dbe394101bdafda548fa01e95
Valentin OMNES <vomnes@student.42.fr>
2017-02-21 19:02:21 +0100

    Update sort.c

--- src/sort.c
+++ src/sort.c
@@ -1,20 +1,26 @@
 # include "push_swap.h"
 
-int ft_lst_min(t_node *lst, t_env *env)
+int ft_lst_min(t_node *lst, t_data *val)
 {
   t_node *temp;
   int index;
 
   temp = lst;
   index = 0;
-  env->data_a.min = 2147483647;
-  env->data_a.index_min = 1;
+  val->min = 2147483647;
+  val->max = -2147483648;
+  val->index_min = 1;
+  val->index_max = 1;
   while (temp != NULL)
   {
-    env->data_a.index_min = (int)temp->data < env->data_a.min ?
-    index : env->data_a.index_min;
-    env->data_a.min = (int)temp->data < env->data_a.min
-    ? (int)temp->data : env->data_a.min;
+    val->index_min = (int)temp->data < val->min ?
+    index : val->index_min;
+    val->min = (int)temp->data < val->min
+    ? (int)temp->data : val->min;
+    val->index_max = (int)temp->data > val->max ?
+    index : val->index_max;
+    val->max = (int)temp->data > val->max
+    ? (int)temp->data : val->max;
     temp = temp->next;
     index++;
   }
@@ -25,12 +31,13 @@ int ft_sort(t_env *env)
 {
   int middle;
   int complexity;
+  t_node *lst;
 
   middle = 0;
   complexity = 0;
   while (env->stack_a != NULL)
   {
-      env->data_a.len = ft_lst_min(env->stack_a, env);
+      env->data_a.len = ft_lst_min(env->stack_a, &env->data_a);
       middle = (int)(env->data_a.len / 2);
   //    ft_printf("middle = %d | len = %d | MIN = %d | index = %d\n",
   //    middle, env->data_a.len, env->data_a.min, env->data_a.index_min);
