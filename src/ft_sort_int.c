106dad2b0208be68c01f08572a023656da9c8e04
Valentin OMNES <vomnes@student.42.fr>
2017-02-22 10:31:48 +0100

    Doesn't work perfectly _ sort int

--- src/ft_sort_int.c
+++ src/ft_sort_int.c
@@ -60,6 +60,7 @@ int	ft_lst_int(t_node *list, t_data *data)
 //  while (i < len)
 //    ft_printf(">> %d\n", tab[i++]);
   data->median = tab[len / 2];
+  ft_putnbr(data->median);
   return (0);
 }
 
@@ -80,99 +81,141 @@ int ft_lst_is_under(t_node *list, int limit)
   return(-1);
 }
 
+int ft_lst_issort(t_node *list)
+{
+  t_node *temp;
+
+  temp = list;
+  while (temp->next != NULL)
+  {
+    if (temp->data > temp->next->data)
+      return (-1);
+    temp = temp->next;
+  }
+  return(1);
+}
+
 int ft_quicksort_insertion(t_env *env)
 {
   int middle;
   int complexity;
   int pos;
+  int flag;
 
   middle = 0;
   complexity = 0;
   pos = -1;
+  flag = 0;
   ft_lst_int(env->stack_a, &env->data_a);
+  ft_printf(">> *%d*\n", env->data_a.median);
   ft_lst_is_under(env->stack_a, 1);
-  ft_printf("|%d|", env->data_a.median);
-  while (env->stack_a != NULL)
+  while (flag == 0)
   {
-      if (ft_lst_is_under(env->stack_a, env->data_a.median) == -1)
-      {
-        ft_lst_int(env->stack_a, &env->data_a);
-        ft_putchar('|');
-        ft_putnbr(env->data_a.median);
-        ft_putchar('|');
-        ft_reverse_rotate(&env->stack_b);
-        ft_putendl("rb");
-        complexity++;
-  //      ft_lst_display(env->stack_b);
-      }
-      while ((pos = ft_lst_is_under(env->stack_a, env->data_a.median)) >= 0)
-      {
-        ft_putnbr(pos);
-        ft_putchar('-');
-        ft_putnbr(env->stack_a->data);
-        middle = (int)(env->data_a.len / 2);
-        if (pos == 0 && env->stack_a->data == env->data_a.median)
+    while (env->stack_a != NULL)
+    {
+        ft_printf(">> *%d*\n", env->data_a.median);
+        if (ft_lst_is_under(env->stack_a, env->data_a.median) == -1)
         {
-          ft_push_ab(&env->stack_a, &env->stack_b);
-          ft_rotate(&env->stack_b);
-          ft_putendl("pb");
-          ft_putendl("rb");
-          complexity += 2;
-        }
-        else if (pos == 0)
-        {
-          ft_push_ab(&env->stack_a, &env->stack_b);
+          ft_lst_int(env->stack_a, &env->data_a);
+          ft_putchar('>');
+          ft_putnbr(env->data_a.median);
+          ft_putchar('<');
+          ft_lst_display(env->stack_a);
+          if (env->stack_b != NULL)
+              ft_reverse_rotate(&env->stack_b);
+          ft_putendl("rrb");
           complexity++;
-          ft_putendl("pb");
+    //      ft_lst_display(env->stack_b);
         }
-        else if (pos < middle)
+        while ((pos = ft_lst_is_under(env->stack_a, env->data_a.median)) >= 0)
         {
-          ft_rotate(&env->stack_a);
-          complexity++;
+          ft_putnbr(pos);
+          ft_putchar('-');
+          ft_putnbr(env->stack_a->data);
+          middle = (int)(env->data_a.len / 2);
+          if (pos == 0 && env->stack_a->data == env->data_a.median)
+          {
+            ft_push_ab(&env->stack_a, &env->stack_b);
+            ft_putendl("pb");
+            ft_rotate(&env->stack_b);
+            ft_putendl("rx");
+            complexity += 2;
+          }
+          else if (pos == 0)
+          {
+            ft_putendl("pb");
+            ft_push_ab(&env->stack_a, &env->stack_b);
+            complexity++;
+          }
+          else if (pos < middle)
+          {
+            ft_putendl("ra");
+            ft_rotate(&env->stack_a);
+            complexity++;
+          }
+          else if (pos >= middle)
+          {
+            ft_putendl("rra");
+            ft_reverse_rotate(&env->stack_a);
+            complexity++;
+          }
+        }
+        flag = 0;
+    }
+    ft_lst_display(env->stack_b);
+    ft_lst_int(env->stack_b, &env->data_b);
+    while (env->stack_b != NULL)
+    {
+        if (ft_lst_is_under(env->stack_b, env->data_b.median) == -1)
+        {
+          ft_lst_int(env->stack_b, &env->data_b);
+          ft_putchar('|');
+          ft_putnbr(env->data_b.median);
+          ft_putchar('|');
+      //    ft_reverse_rotate(&env->stack_a);
           ft_putendl("ra");
-        }
-        else if (pos >= middle)
-        {
-          ft_reverse_rotate(&env->stack_a);
           complexity++;
-          ft_putendl("rra");
+        //      ft_lst_display(env->stack_b);
         }
-      }
-  }
-  ft_lst_display(env->stack_b);
-  while (env->stack_b != NULL)
-  {
-        env->data_b.len = ft_lst_min(env->stack_b, &env->data_b);
-        middle = (int)(env->data_b.len / 2);
-        ft_printf("middle = %d | len = %d | MAX = %d | index = %d\n",
-        middle, env->data_b.len, env->data_b.max, env->data_b.index_max);
-        if (env->data_b.index_max == 0)
+        while ((pos = ft_lst_is_under(env->stack_b, env->data_b.median)) >= 0)
         {
-          ft_push_ab(&env->stack_b, &env->stack_a);
-          complexity++;
-          ft_putendl("pa");
+          ft_putnbr(pos);
+          ft_putchar('-');
+          ft_putnbr(env->stack_b->data);
+          middle = (int)(env->data_b.len / 2);
+          if (pos == 0 && env->stack_b->data == env->data_b.median)
+          {
+            ft_push_ab(&env->stack_b, &env->stack_a);
+            ft_rotate(&env->stack_a);
+            ft_putendl("pa");
+            ft_putendl("ra");
+            complexity += 2;
+          }
+          else if (pos == 0)
+          {
+            ft_push_ab(&env->stack_b, &env->stack_a);
+            complexity++;
+            ft_putendl("pa");
+          }
+          else if (pos < middle)
+          {
+            ft_rotate(&env->stack_b);
+            complexity++;
+            ft_putendl("rb");
+          }
+          else if (pos >= middle)
+          {
+            ft_reverse_rotate(&env->stack_b);
+            complexity++;
+            ft_putendl("rrb");
+          }
         }
-        else if (env->data_b.index_max == 1)
-        {
-          ft_swap_one_two(&env->stack_b);
-          complexity++;
-          ft_putendl("sa");
-        }
-        else if (env->data_a.index_max < middle)
-        {
-          ft_rotate(&env->stack_b);
-          complexity++;
-          ft_putendl("ra");
-        }
-        else if (env->data_a.index_max >= middle)
-        {
-          ft_reverse_rotate(&env->stack_b);
-          complexity++;
-          ft_putendl("rra");
-        }
-        ft_putendl("<<>>");
-        ft_lst_display(env->stack_b);
-        break ;
+        flag = 0;
+    }
+    if (env->stack_a != NULL && env->stack_b == NULL && ft_lst_issort(env->stack_a) == -1)
+        flag = 1;
+    else
+        flag = 0;
   }
   ft_lst_display(env->stack_a);
 //  ft_printf("len = %d | MIN = %d | index = %d\n",
