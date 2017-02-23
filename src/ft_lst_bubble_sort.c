d3bf2fe9163cef659269aef7df8b39e75f28d784
Valentin OMNES <vomnes@student.42.fr>
2017-02-23 10:35:05 +0100

    bubble_sort OK with 3/2 need other for 5/6

--- src/ft_lst_bubble_sort.c
+++ src/ft_lst_bubble_sort.c
@@ -12,74 +12,79 @@
 
 # include "push_swap.h"
 
-int ft_lst_bubble_sort(t_node *stack_a, t_node *stack_b)
+int ft_lst_basic_bubble_sort(t_node **stack_a, t_node **stack_b)
 {
   t_node *tmp;
-  int complexity;
 
-  if (stack_a->next == NULL)
+  if ((*stack_a)->next == NULL)
       return (1);
-  tmp = stack_a;
-  complexity = 0;
-  ft_print_stacks(stack_a, stack_b);
-  while (ft_lst_is_sorted(stack_a) == -1)
+  tmp = *stack_a;
+  while (ft_lst_is_sorted(*stack_a) == -1)
   {
     if (tmp->data < tmp->next->data)
     {
-      ft_swap_one_two(&stack_a);
+      ft_swap_one_two(&(*stack_a));
       ft_putendl("sa");
-      ft_print_stacks(stack_a, stack_b);
-
-      ft_rotate(&stack_a);
+      ft_rotate(&(*stack_a));
       ft_putendl("ra");
-      complexity += 2;
-      ft_print_stacks(stack_a, stack_b);
     }
     else
     {
-      ft_rotate(&stack_a);
+      ft_rotate(&(*stack_a));
       ft_putendl("ra");
-      complexity += 1;
-      ft_print_stacks(stack_a, stack_b);
     }
   }
-  ft_print_stacks(stack_a, stack_b);
-  ft_printf(BLUE">> %d\n"RESET, complexity);
   return (1);
 }
 
-int ft_lst_rev_bubble_sort(t_node *stack_a, t_node *stack_b)
+int ft_lst_rev_bubble_sort(t_node **stack_a, t_node **stack_b)
 {
   t_node *tmp;
-  int complexity;
 
-  if (stack_a->next == NULL)
+  if ((*stack_a)->next == NULL)
       return (1);
-  tmp = stack_a;
-  complexity = 0;
-  ft_print_stacks(stack_a, stack_b);
-  while (ft_lst_is_sorted(stack_a) == -1)
+  tmp = *stack_a;
+  while (ft_lst_is_sorted(*stack_a) == -1)
   {
     if (tmp->data > tmp->next->data)
     {
-      ft_swap_one_two(&stack_a);
+      ft_swap_one_two(&(*stack_a));
       ft_putendl("sa");
-      ft_print_stacks(stack_a, stack_b);
-
-      ft_reverse_rotate(&stack_a);
-      ft_putendl("ra");
-      complexity += 2;
-      ft_print_stacks(stack_a, stack_b);
+      ft_reverse_rotate(&(*stack_a));
+      ft_putendl("rra");
     }
     else
     {
-      ft_reverse_rotate(&stack_a);
-      ft_putendl("ra");
-      complexity += 1;
-      ft_print_stacks(stack_a, stack_b);
+      ft_reverse_rotate(&(*stack_a));
+      ft_putendl("rra");
     }
   }
-  ft_print_stacks(stack_a, stack_b);
-  ft_printf(BLUE">> %d\n"RESET, complexity);
   return (1);
+}
+
+void ft_lst_bubble_sort(t_node *stack_a, t_node *stack_b)
+{
+  int min;
+  int max;
+
+  min = 0;
+  max = 0;
+  if (ft_lst_len(stack_a) <= 2)
+      ft_lst_basic_bubble_sort(&stack_a, &stack_b);
+  if (ft_lst_len(stack_a) == 3)
+  {
+      min = ft_lst_min(stack_a);
+      max = ft_lst_max(stack_a);
+      if (stack_a->next->next->data == min)
+        ft_lst_rev_bubble_sort(&stack_a, &stack_b);
+      else if (stack_a->next->data == min &&
+      stack_a->next->next->data == max)
+      {
+        ft_swap_one_two(&stack_a);
+        ft_putendl("sa");
+      }
+      else
+        ft_lst_basic_bubble_sort(&stack_a, &stack_b);
+  }
+  ft_print_stacks(stack_a, stack_b);
 }
