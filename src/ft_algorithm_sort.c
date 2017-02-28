e1f358b57609ec86fc790991dad4898c66afc5d7
Valentin OMNES <vomnes@student.42.fr>
2017-02-28 19:44:48 +0100

    Algo ok clean code

--- src/ft_algorithm_sort.c
+++ src/ft_algorithm_sort.c
@@ -12,6 +12,10 @@
 
 # include "push_swap.h"
 
+# define MAX val.max
+# define MAX_2 val.max_2
+# define MAX_3 val.max_3
+
 static int ft_push_max(t_node **stack_a, t_node **stack_b, int pos, int middle)
 {
   if (pos == 0)
@@ -82,8 +86,6 @@ int ft_get_true_index_value(t_node *stack, int nb)
 
   tmp = stack;
   index = 0;
-//  if (tmp->data == nb)
-//      return (0);
   while (tmp != NULL && tmp->data != nb)
   {
     tmp = tmp->next;
@@ -119,118 +121,101 @@ void ft_push_selected_value(t_node **stack_a, t_node **stack_b, int nb)
     }
 }
 
-int ft_algorithm_sort(t_env *env)
+void ft_algo_opti_max(t_node **stack_a, t_node **stack_b, t_env *env)
 {
-  int middle;
-  int pos;
-  int flag;
-  int turn;
-  int is_sorted;
-  int level;
+  t_max val;
+
+  MAX = env->data_b.max;
+  MAX_2 = ft_max_under_max(*stack_b, MAX);
+  MAX_3 = ft_max_under_max(*stack_b, MAX_2);
+  if (ft_get_index_value(*stack_b, MAX_2) < ft_get_index_value(*stack_b, MAX) &&
+  ft_get_index_value(*stack_b, MAX_2) < ft_get_index_value(*stack_b, MAX_3))
+  {
+    ft_push_selected_value(&(*stack_a), &(*stack_b), MAX_2);
+    ft_push_selected_value(&(*stack_a), &(*stack_b), MAX);
+    ft_swap_one_two(&(*stack_a));
+    ft_putendl("sa");
+  }
+  else if (
+  ft_get_index_value(*stack_b, MAX_3) < ft_get_index_value(*stack_b, MAX) &&
+  ft_get_index_value(*stack_b, MAX_2) > ft_get_index_value(*stack_b, MAX_3))
+  {
+    ft_push_selected_value(&(*stack_a), &(*stack_b), MAX_3);
+    ft_push_selected_value(&(*stack_a), &(*stack_b), MAX);
+    ft_swap_one_two(&(*stack_a));
+    ft_putendl("sa");
+    ft_push_selected_value(&(*stack_a), &(*stack_b), MAX_2);
+    ft_swap_one_two(&(*stack_a));
+    ft_putendl("sa");
+  }
+  else
+    ft_push_selected_value(&(*stack_a), &(*stack_b), MAX);
+}
+
+void ft_push_on_b(t_node **stack_a, t_node **stack_b, t_data *data_a, int o_len)
+{
   int len;
+  int level;
+  int pos;
+  int middle;
+
+  len = 0;
+  level = 2;
+  pos = 0;
+  middle = 0;
+  if ((pos = ft_lst_is_under(*stack_a, data_a->median)) < 0)
+  {
+    len = ft_lst_len(*stack_a);
+    if (o_len >= 1000)
+        level = ((1.0 / 75.0) * len + 5.0 / 2.9) + 0.5;
+    else
+        level = ((1.0 / 62.0) * len + 4.8 / 2.3) + 0.5;
+    level = (level < 2) ? 2 : level;
+    ft_lst_median(*stack_a, &(*data_a), level);
+  }
+  pos = ft_lst_is_under(*stack_a, data_a->median);
+  middle = (int)(data_a->len / 2);
+  ft_push_min(&(*stack_a), &(*stack_b), pos, middle);
+}
+
+static void ft_push_on_b_while(t_node **stack_a, t_node **stack_b, t_env *env,
+int o_len)
+{
+  while (2)
+  {
+      if ((ft_lst_len(*stack_a) == 3 &&
+      ft_lst_min(*stack_a) > ft_lst_max(*stack_b)) ||
+      (ft_lst_len(*stack_a) == 2))
+      {
+        ft_big_bubble_sort(&(*stack_a), &(*stack_b), env);
+        break ;
+      }
+      ft_push_on_b(&(*stack_a), &(*stack_b), &env->data_a, o_len);
+  //    ft_print_stacks(env->stack_a, env->stack_b); usleep(250000);
+  }
+}
+
+void ft_algorithm_sort(t_env *env)
+{
+  int level;
   int original_len;
 
-  int max;
-  int max_2;
-  int max_3;
-
-  middle = 0;
-  pos = -1;
-  flag = 0;
-  turn = 0;
-  len = ft_lst_len(env->stack_a);
   original_len = ft_lst_len(env->stack_a);
-  level = ((1.0 / 75.0) * len + 5.0 / 2.9) + 0.5;
+  level = ((1.0 / 75.0) * original_len + 5.0 / 2.9) + 0.5;
   level = (level < 2) ? 2 : level;
   ft_lst_median(env->stack_a, &env->data_a, level);
-  is_sorted = 0;
-  while (is_sorted == 0)
+  while (1)
   {
-    while (turn == 0)
-    {
-        if (ft_lst_len(env->stack_a) == 3 &&
-        ft_lst_min(env->stack_a) > ft_lst_max(env->stack_b))
-        {
-          ft_big_bubble_sort(&env->stack_a, &env->stack_b, env);
-          turn = 1;
-          break ;
-        }
-        if (ft_lst_len(env->stack_a) == 2)
-        {
-          ft_big_bubble_sort(&env->stack_a, &env->stack_b, env);
-          turn = 1;
-          break ;
-        }
-        if (ft_lst_is_sorted(env->stack_a) == 1 &&
-        (env->stack_a->data > ft_lst_max(env->stack_b)))
-        {
-          turn = 1;
-          break ;
-        }
-        if ((pos = ft_lst_is_under(env->stack_a, env->data_a.median)) < 0)
-        {
-          len = ft_lst_len(env->stack_a);
-          if (original_len >= 1000)
-              level = ((1.0 / 75.0) * len + 5.0 / 2.9) + 0.5;
-          else
-              level = ((1.0 / 60.0) * len + 5.0 / 2.3) + 0.5;
-          level = (level < 2) ? 2 : level;
-          ft_lst_median(env->stack_a, &env->data_a, level);
-        }
-        pos = ft_lst_is_under(env->stack_a, env->data_a.median);
-        middle = (int)(env->data_a.len / 2.0);
-        ft_push_min(&env->stack_a, &env->stack_b, pos, middle);
-    //    ft_print_stacks(env->stack_a, env->stack_b); usleep(250000);
-    }
-    while (turn == 1)
+    ft_push_on_b_while(&env->stack_a, &env->stack_b, env, original_len);
+    while (3)
     {
         if (env->stack_b == NULL)
-        {
-          turn = 0;
-          break ;
-        }
-            ft_lst_values(env->stack_b, &env->data_b);
-            max = env->data_b.max;
-            max_2 = ft_max_under_max(env->stack_b, max);
-            max_3 = ft_max_under_max(env->stack_b, max_2);
-            if ((max > max_2) && ft_get_index_value(env->stack_b, max_2) <
-            ft_get_index_value(env->stack_b, env->data_b.max) &&
-            ft_get_index_value(env->stack_b, max_2) < ft_get_index_value(env->stack_b, max_2))
-            {
-              ft_push_selected_value(&env->stack_a, &env->stack_b, max_2);
-              ft_push_selected_value(&env->stack_a, &env->stack_b, max);
-              ft_swap_one_two(&env->stack_a);
-              ft_putendl("sa");
-            }
-            else if ((max_3 < max) &&
-            ft_get_index_value(env->stack_b, max_3) < ft_get_index_value(env->stack_b, max) &&
-            ft_get_index_value(env->stack_b, max_2) > ft_get_index_value(env->stack_b, max_3))
-            {
-              ft_push_selected_value(&env->stack_a, &env->stack_b, max_3);
-              ft_push_selected_value(&env->stack_a, &env->stack_b, max);
-              ft_swap_one_two(&env->stack_a);
-              ft_putendl("sa");
-              ft_push_selected_value(&env->stack_a, &env->stack_b, max_2);
-              ft_swap_one_two(&env->stack_a);
-              ft_putendl("sa");
-            }
-            else
-            {
-            //  ft_putendl("Last");
-            //  ft_putnbr(max);
-              ft_push_selected_value(&env->stack_a, &env->stack_b, max);
-            } /* Work in progress */
-        //ft_lst_values(env->stack_b, &env->data_b);
-        //pos = env->data_b.index_max;
-        //middle = (int)(ft_lst_len(env->stack_b) / 2);
-        //ft_push_max(&env->stack_a, &env->stack_b, pos, middle);
+            break ;
+        ft_lst_values(env->stack_b, &env->data_b);
+        ft_algo_opti_max(&env->stack_a, &env->stack_b, env);
         //ft_print_stacks(env->stack_a, env->stack_b); //usleep(250000);
     }
     if (ft_lst_is_sorted(env->stack_a) == 1 && env->stack_b == NULL)
-    {
-      is_sorted = 1;
       break ;
-    }
   }
-  return (0);
 }
